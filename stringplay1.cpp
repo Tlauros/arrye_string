62f98a2654d618005dde2ae352debec581db39eb
Tlauros <105284032+Tlauros@users.noreply.github.com>
2022-05-10 15:11:44 +0430

    Add files via upload
    my fist code and first upload :)

---
+++ stringplay1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string Name;
+    cout << "enter your name :";
+    cin >> Name;
+    int i;
+    cout << "start \n";
+    cout << "----------------------------------------------------- \n";
+    for ( i = 0; i < Name.size(); i++){
+        cout << i << " -> ";
+        cout << Name[i] << endl; 
+    }
+    cout << "----------------------------------------------------- \n";
+    cout << "finish";
+
+    return 0;
+}
